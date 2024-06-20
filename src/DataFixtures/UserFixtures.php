f72afdddb95248dfdcb9ccf0aa8923b0a386f262
benjaminroche <benjamin.roche4@gmail.com>
2024-06-20 14:05:41 +0200

    feat: Users (entity + fixtures)

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword('$2y$13$' . $faker->sha256);
+            $user->setCreatedAt(new \DateTimeImmutable());
+            $user->setFirstName($faker->firstName);
+            $user->setLastName($faker->lastName);
+            $user->setRoles(['ROLE_USER']);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
