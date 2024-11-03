0939db04e80108ee0105cd5f44245a5fb835b1f1
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2024-11-03 23:59:10 +0100

    fix: add a state for isNew and compute it inside a useEffect

--- src/components/new-badge.tsx
+++ src/components/new-badge.tsx
@@ -3,11 +3,18 @@
 import { differenceInDays } from 'date-fns'
 import { useTranslations } from 'next-intl'
 import * as React from 'react'
+import { useEffect, useState } from 'react'
 import { Badge } from '@/components/ui/badge'
+
+const MAX_DAYS_FOR_NEW = 5
 
 export const NewBadge = ({ date }: { date: Date }) => {
   const t = useTranslations()
-  const isNew = differenceInDays(new Date(), date) <= 5
+  const [isNew, setIsNew] = useState(false)
+
+  useEffect(() => {
+    setIsNew(differenceInDays(new Date(), date) <= MAX_DAYS_FOR_NEW)
+  }, [date])
 
   if (isNew) {
     return <Badge variant="outline">{t('Common.new')}</Badge>
