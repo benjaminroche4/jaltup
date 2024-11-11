679ab74fd03a5ca7a3c1bf953b729748aaffd4fc
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2024-11-11 11:26:27 +0100

    feat: split error page in two components

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -4,30 +4,34 @@ import Link from 'next/link'
 import { useTranslations } from 'next-intl'
 import * as React from 'react'
 
-export const ErrorPage = (): React.ReactNode => {
+const ErrorContent = () => {
   const t = useTranslations()
 
   return (
-    <section className="relative z-10 bg-sky-600 py-[120px]">
-      <div className="container mx-auto">
-        <div className="-mx-4 flex">
-          <div className="w-full px-4">
-            <div className="mx-auto max-w-[400px] text-center">
-              <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
-                {t('ErrorPage.title')}
-              </h4>
-              <p className="mb-8 text-lg text-white">{t('ErrorPage.text')}</p>
-              <Link
-                href="/"
-                className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white
-                  transition hover:bg-white hover:text-primary"
-              >
-                {t('Common.back')}
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div className="mx-auto max-w-[400px] text-center">
+      <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
+        {t('ErrorPage.title')}
+      </h4>
+      <p className="mb-8 text-lg text-white">{t('ErrorPage.text')}</p>
+      <Link
+        href="/"
+        className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white
+          transition hover:bg-white hover:text-primary"
+      >
+        {t('Common.back')}
+      </Link>
+    </div>
   )
 }
+
+export const ErrorPage = (): React.ReactNode => (
+  <section className="relative z-10 bg-sky-600 py-[120px]">
+    <div className="container mx-auto">
+      <div className="-mx-4 flex">
+        <div className="w-full px-4">
+          <ErrorContent />
+        </div>
+      </div>
+    </div>
+  </section>
+)
