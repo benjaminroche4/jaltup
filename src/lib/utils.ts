4952623dc4890b6d2d5a6c8a994e70fd86bb899d
Alexandre Costantini <alexandre.costantini@exoalpha.com>
2024-11-30 18:09:58 +0100

    feat: add a subscribe/register page for step 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,35 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const stringValidator = (str: string, min = 2, max = 50) =>
+  str.length >= min && str.length <= max
+
+export const emailValidator = (str: string) => {
+  if (!stringValidator(str, 2, 50)) {
+    return false
+  }
+
+  return str
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    )
+}
+
+export const passwordValidator = (str: string) => {
+  if (!stringValidator(str, 6, 20)) {
+    return false
+  }
+
+  const numbers = str.match(/[0-9]/g)
+  if (numbers === null || numbers.length < 2) {
+    return false
+  }
+
+  const specials = str.match(/[^0-9A-Za-z]/g)
+  if (specials === null || specials.length < 2) {
+    return false
+  }
+  return true
+}
